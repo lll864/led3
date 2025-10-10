f5b7241bd96e42b393720ba2e287064e5fb345cc
lll864 <3375763141@qq.com>
2025-10-10 17:27:59 +0800

    zhongduan

---
+++ MDK-ARM/RTE/_led3/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'led3' 
+ * Target:  'led3' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
